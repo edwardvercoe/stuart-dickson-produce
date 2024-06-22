2ec868ddf8ca5ce91015cb1a025be76faf07d73d
mv <madeleine.vercoe@gmail.com>
2024-06-22 10:55:03 +1000

    finished home hero

--- components/shared/Hero/HomeHero.tsx
+++ components/shared/Hero/HomeHero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import SanityImg from '@/components/SanityComponents/SanityImg'
 import PortableTextBlock from '@/components/shared/PortableText/PortableTextBlock'
+import Button from '@/components/shared/Button'
 
 type HomeHeroProps = {
   data: any
@@ -12,20 +13,28 @@ const HomeHero = ({ data }: HomeHeroProps) => {
   const { title, subtitle, backgroundImage, buttons } = data
 
   return (
-    <div>
-      HomeHero component: HomeHero.tsx
-      <h1>title: {title}</h1>
+    <div className='flex' style={{backgroundColor: '#EAE3DD'
+    }}>
+      <div className='flex-1 p-6'>
+      <h1 className="h1 text-6xl font-extrabold leading-6xl text-left -mb-4">{title}</h1>
       <br />
-      <p>subtitle:</p>
       <PortableTextBlock data={subtitle} />
-      <br />
-      <p>image:</p>
+      {buttons && (
+        <div className="flex gap-4 mt-4">
+          {buttons.map((button, index) => (
+            <a key={index} href={button.internalLink} className={index === 0 ? 'button' : 'button button--secondary'}>{button.linkText}</a>
+          ))}
+        </div>
+      )}
+      </div>
+      <div className='flex-1'>
       <figure className="relative">
         <SanityImg
           src={backgroundImage}
           className="w-full h-full object-cover"
         />
       </figure>
+      </div>
     </div>
   )
 }
