dbf45127c57a6d487cc19acd410d9f67470b696f
mv <madeleine.vercoe@gmail.com>
2024-06-23 21:13:06 +1000

    feateredcta updates

--- components/shared/FeaturedCTA.tsx
+++ components/shared/FeaturedCTA.tsx
@@ -1,32 +1,51 @@
 import React from 'react'
-
-import SanityImg from '@/components/SanityComponents/SanityImg'
 import PortableTextBlock from '@/components/shared/PortableText/PortableTextBlock'
+import imageUrlBuilder from '@sanity/image-url';
+import { client } from '@/sanity/lib/client';
+
+const builder = imageUrlBuilder(client);
+  
+function urlFor(source) {
+  return builder.image(source);
+}
 
 type FeaturedCTAProps = {
   data: any
 }
 
+
 const FeaturedCTA = ({ data }: FeaturedCTAProps) => {
-  const { title, description, backgroundImage, buttons } = data
+  console.log(data);
 
-  return (
-    <div>
-      Featured CTA component: FeaturedCTA.tsx
-      <h1>title: {title}</h1>
-      <br />
-      <p>description:</p>
-      <PortableTextBlock data={description} />
-      <br />
-      <p>backgroundImage:</p>
-      <figure className="relative">
-        <SanityImg
-          src={backgroundImage}
-          className="w-full h-full object-cover"
-        />
-      </figure>
-    </div>
-  )
-}
-
+  const { title, backgroundImage, buttons, description } = data;  
+    return (
+      <div 
+        className="flex p-6 bg-cover bg-center items-center bg-white" 
+        style={{
+          minHeight: '600px',
+          backgroundImage: `url(${urlFor(backgroundImage.asset._ref)})`
+        }}
+      >
+        <div className='md:w-1/2  text-white '>
+          <h1 className="h1 text-6xl font-extrabold leading-6xl text-left -mb-4">{title}</h1>
+          <br />
+          <PortableTextBlock data={description} />
+          {buttons && (
+            <div className="flex gap-4 mt-4">
+              {buttons.map((button, index) => (
+                <a 
+                  key={index}
+                  href={button.internalLink} 
+                  className={`px-4 py-2 cursor-pointer shadow-md bg-white text-black border border-black'}`}
+                >
+                  {button.linkText}
+                </a>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
+    
 export default FeaturedCTA
--- components/shared/Hero/HomeHero.tsx
+++ components/shared/Hero/HomeHero.tsx
@@ -1,41 +1,50 @@
-import React from 'react'
+import React from 'react';
+import PortableTextBlock from '@/components/shared/PortableText/PortableTextBlock';
+import imageUrlBuilder from '@sanity/image-url';
+import { client } from '@/sanity/lib/client';
 
-import SanityImg from '@/components/SanityComponents/SanityImg'
-import PortableTextBlock from '@/components/shared/PortableText/PortableTextBlock'
+const builder = imageUrlBuilder(client);
+
+function urlFor(source) {
+  return builder.image(source);
+}
 
 type HomeHeroProps = {
-  data: any
-}
+  data: any;
+};
 
 const HomeHero = ({ data }: HomeHeroProps) => {
-  console.log(data)
-  const { title, subtitle, backgroundImage, buttons } = data
+  const { title, subtitle, backgroundImage, buttons } = data;
 
   return (
-    <div className='flex' style={{backgroundColor: '#EAE3DD'
-    }}>
-      <div className='flex-1 p-6'>
-      <h1 className="h1 text-6xl font-extrabold leading-6xl text-left -mb-4">{title}</h1>
-      <br />
-      <PortableTextBlock data={subtitle} />
-      {buttons && (
-        <div className="flex gap-4 mt-4">
-          {buttons.map((button, index) => (
-            <a key={index} href={button.internalLink} className={index === 0 ? 'button' : 'button button--secondary'}>{button.linkText}</a>
-          ))}
-        </div>
-      )}
-      </div>
-      <div className='flex-1'>
-      <figure className="relative">
-        <SanityImg
-          src={backgroundImage}
-          className="w-full h-full object-cover"
-        />
-      </figure>
+    <div 
+      className="flex p-6 bg-auto bg-no-repeat bg-right items-center" 
+      style={{
+        minHeight: '600px',
+        backgroundColor: '#EAE3DD',
+        backgroundImage: `url(${urlFor(backgroundImage.asset._ref)})`
+      }}
+    >
+      <div className='md:w-1/2'>
+        <h1 className="h1 text-6xl font-extrabold leading-6xl text-left -mb-4">{title}</h1>
+        <br />
+        <PortableTextBlock data={subtitle} />
+        {buttons && (
+          <div className="flex gap-4 mt-4">
+            {buttons.map((button, index) => (
+              <a 
+                key={index}
+                href={button.internalLink} 
+                className={`px-4 py-2 cursor-pointer shadow-md ${index === 0 ? 'bg-custom-orange text-white' : 'bg-none text-black border border-custom-orange'}`}
+              >
+                {button.linkText}
+              </a>
+            ))}
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeHero
+export default HomeHero;
