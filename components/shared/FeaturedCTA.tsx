99a9e79bf23075d4b5cc62e166e8d0b1ca11e1ea
Edward Vercoe <edward.vercoe@gmail.com>
2024-07-17 11:38:32 +1000

    make home page look nicer

--- components/shared/FeaturedCTA.tsx
+++ components/shared/FeaturedCTA.tsx
@@ -14,6 +14,7 @@ const FeaturedCTA = ({ data }: FeaturedCTAProps) => {
     backgroundImage,
     buttons,
     description,
+    foregroundImage,
     variation = 'left',
   } = data
   return (
@@ -24,10 +25,21 @@ const FeaturedCTA = ({ data }: FeaturedCTAProps) => {
           className="w-full h-full object-cover object-center"
         />
       </figure>
+      {foregroundImage && (
+        <figure className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[20vw] h-[20vw] shadow-2xl">
+          <SanityImg
+            loading="eager"
+            src={foregroundImage}
+            className="w-full h-full object-cover object-center"
+          />
+        </figure>
+      )}
       <div className="flex p-6 bg-cover bg-center items-center min-h-[600px] relative">
         <div className="md:w-1/2 text-white relative">
-          <h1 className="text-6xl font-extrabold text-left">{title}</h1>
-          <div>
+          <div className="pb-6">
+            <h1 className="text-6xl font-extrabold text-left">{title}</h1>
+          </div>
+          <div className="max-w-[520px]">
             <PortableTextBlock data={description} />
           </div>
           {buttons && (
--- components/shared/Hero/HomeHero.tsx
+++ components/shared/Hero/HomeHero.tsx
@@ -11,49 +11,70 @@ type HomeHeroProps = {
 }
 
 const HomeHero = ({ data }: HomeHeroProps) => {
-  const { title, subtitle, backgroundImage, buttons } = data
+  const { title, subtitle, backgroundImage, buttons, foregroundImage } = data
   return (
-    <section className="full-bleed">
-      <figure className="absolute top-0 left-0 w-full h-full  gradient-left">
-        <SanityImg
-          loading="eager"
-          src={backgroundImage}
-          className="w-full h-full object-cover object-center"
-        />
-      </figure>
-      <div className="flex p-6 bg-auto bg-no-repeat bg-right items-center min-h-[800px] h-[100vh] relative">
-        <div className="md:w-1/2 relative">
-          <h1 className="h1 text-6xl font-extrabold leading-6xl text-left -mb-4 text-white">
-            {title}
-          </h1>
-          <br />
-          <PortableTextBlock className="text-white" data={subtitle} />
-          {buttons && (
-            <div className="flex gap-4 mt-4">
-              {buttons.map((button, index) => (
-                <SanityLink data={button} key={button._key}>
-                  <Button
-                    variant={index === 0 ? 'primary' : 'secondary'}
-                    iconRight={
-                      index === 0 && (
-                        <Image
-                          src={BrandArrowAccent}
-                          alt="brand arrow icon"
-                          height={16}
-                          width={16}
-                        />
-                      )
-                    }
-                  >
-                    {button.linkText}
-                  </Button>
-                </SanityLink>
-              ))}
+    <>
+      <section className="full-bleed">
+        <figure className="absolute top-0 left-0 w-full h-full  gradient-left">
+          <SanityImg
+            loading="eager"
+            src={backgroundImage}
+            className="w-full h-full object-cover object-center"
+          />
+        </figure>
+        {foregroundImage && (
+          <figure className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[20vw] h-[20vw] shadow-2xl">
+            <SanityImg
+              loading="eager"
+              src={foregroundImage}
+              className="w-full h-full object-cover object-center"
+            />
+          </figure>
+        )}
+        <div className="flex pb-4 md:p-6 items-center min-h-[800px] h-[100vh] relative">
+          <div className="w-full max-w-[560px] relative">
+            <div className="pb-8">
+              <h1 className="h1 text-6xl font-extrabold leading-6xl text-left -mb-4 text-white">
+                {title}
+              </h1>
             </div>
-          )}
+
+            <PortableTextBlock
+              className="text-white pb-8 max-w-[520px]"
+              data={subtitle}
+            />
+            {buttons && (
+              <div className="flex gap-4 mt-4">
+                {buttons.map((button, index) => (
+                  <SanityLink data={button} key={button._key}>
+                    <Button
+                      variant={index === 0 ? 'primary' : 'secondary'}
+                      iconRight={
+                        index === 0 && (
+                          <Image
+                            src={BrandArrowAccent}
+                            alt="brand arrow icon"
+                            height={16}
+                            width={16}
+                          />
+                        )
+                      }
+                    >
+                      {button.linkText}
+                    </Button>
+                  </SanityLink>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="full-bleed h-4 flex flex-row relative w-full z-10">
+        <div className="bg-[#17601A] flex-1" />
+        <div className="bg-[#17601A]/60 flex-1" />
+        <div className="bg-[#D8EDD9] flex-1" />
+      </section>
+    </>
   )
 }
 
