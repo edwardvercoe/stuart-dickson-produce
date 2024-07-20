da7f74f4e54f76062262cfa0c484ef1cf3e627c6
mv <madeleine.vercoe@gmail.com>
2024-07-20 17:08:30 +1000

    updated component to design specs

--- components/shared/TwoColText/TwoColTextHalf.tsx
+++ components/shared/TwoColText/TwoColTextHalf.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PortableTextBlock from '../PortableText/PortableTextBlock'
 
 type TwoColTextProps = {
   data: any
@@ -8,9 +9,32 @@ const TwoColTextHalf = ({ data }: TwoColTextProps) => {
   const { variation, leftColumn, rightColumn } = data
 
   return (
-    <section className="py-10">
-      <div>TwoColTextHalf.tsx</div>
-    </section>
+  <section className="py-10">
+      <div>
+        <div className="flex flex-col md:flex-row ">
+        <div className="w-full md:w-1/2 flex-col flex text-3xl">
+  {leftColumn.map((item, index) => (
+    <div
+      key={index}
+      className={
+        index === 0
+          ? 'flex items-center text-brand-darker-green'
+          : 'text-brand-gray'
+      }
+    >
+      {index === 0 && <span className="text-orange-500 mr-2">||</span>}
+      <PortableTextBlock data={item} />
+    </div>
+  ))}
+</div>
+
+          <div className="w-full md:w-1/2">
+       
+                <PortableTextBlock data={rightColumn} className='text-3xl lg:text-3xl text-brand-darker-green pb-8'/>
+              </div>
+          </div>
+        </div>
+    </section>  
   )
 }
 
