69df764df796e0e720838c61d803073de24c9096
mv <madeleine.vercoe@gmail.com>
2024-07-21 21:05:34 +1000

    formatting

--- components/shared/TwoImages/TwoImagesApart.tsx
+++ components/shared/TwoImages/TwoImagesApart.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import PortableTextBlock from '../PortableText/PortableTextBlock'
+import SanityImg from '@/components/SanityComponents/SanityImg'
 
 type TwoImagesProps = {
   data: any
@@ -8,7 +10,27 @@ const TwoImagesApart = ({ data }: TwoImagesProps) => {
   const { imageLeft, imageRight, text } = data
   return (
     <section>
-      <div>TwoImagesApart.tsx</div>
+      <div className='flex flex-row'>
+      <div className='flex flex-col w-1/2'>
+      {text.map((item, index) => (
+    <div
+      key={index}
+      className={
+        index === 0
+          ? 'flex text-brand-darker-green'
+          : 'text-brand-gray'
+      }
+    >
+      {index === 0 && <span className="text-orange-500 mr-2">||</span>}
+      <PortableTextBlock data={text} />
+    </div>
+  ))}
+  <SanityImg src={imageLeft} className="w-1/2 h-full object-cover" />
+           </div>
+                <div className='w-1/2 justify-end'>
+        <SanityImg src={imageRight} className="w-3/4 h-4/5 object-cover" />           
+        </div>
+  </div>
     </section>
   )
 }
