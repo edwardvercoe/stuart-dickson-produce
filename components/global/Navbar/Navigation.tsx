af40ee56222b5760a392d188a5853bc84230a741
Frank Spin <info@frankspin.nl>
2025-03-24 11:20:38 +0100

    fix: update Navigation component to use new menu icon

--- components/global/Navbar/Navigation.tsx
+++ components/global/Navbar/Navigation.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 
 import crest262 from '@/assets/img/crest-262.png'
 import SanityLink from '@/components/SanityComponents/SanityLink'
-import type { MenuItem, SettingsPayload } from '@/types'
+import type { SettingsPayload } from '@/types'
 
 import NavMenu from './NavMenu'
 import SpinningLogo from './SpinningLogo'
@@ -13,27 +13,29 @@ import SpinningLogo from './SpinningLogo'
 interface NavbarProps {
   data: SettingsPayload
 }
+
 export default function Navbar(props: NavbarProps) {
   const { data } = props
-  const menuItems = data?.menuItems || ([] as MenuItem[])
+  const menuItems = data?.menuItems || []
+  
   // Function to insert empty objects between menu items
-  const insertEmptyObjects = (items: MenuItem[]): MenuItem[] => {
+  const insertEmptyObjects = (items: NonNullable<SettingsPayload['menuItems']>): Array<NonNullable<SettingsPayload['menuItems']>[number] | { isSpacerItem: true }> => {
     return items.reduce((acc, item, index) => {
       acc.push(item)
       if (index < items.length - 1) {
-        acc.push({} as MenuItem) // Cast empty object to MenuItem
+        acc.push({ isSpacerItem: true })
       }
       return acc
-    }, [] as MenuItem[])
+    }, [] as Array<NonNullable<SettingsPayload['menuItems']>[number] | { isSpacerItem: true }>)
   }
+  
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  // Assign newMenuItems with the correct type
-  const newMenuItems: MenuItem[] = insertEmptyObjects(menuItems)
+  const newMenuItems = insertEmptyObjects(menuItems)
 
   return (
-    <nav className="fixed w-full top-0 left-0 z-[999]  py-4 ">
-      <div className="flex justify-between items-center w-full">
+    <nav className="fixed w-full top-0 left-0 z-[999] py-4">
+      <div className="container mx-auto  flex justify-between items-center">
         <div className="w-1/6">
           <Link href="/" className="relative block w-[90px] h-[90px]">
             <Image
@@ -43,21 +45,21 @@ export default function Navbar(props: NavbarProps) {
               height={90}
               className="absolute top-0 left-0 z-10"
             />
-
             <SpinningLogo />
           </Link>
         </div>
-        {menuItems && (
+        
+        {menuItems && menuItems.length > 0 && (
           <div className="w-4/6 hidden md:block">
-            <div className="flex gap-4 items-center justify-center w-auto rounded-full bg-brand-black/60  text-white backdrop-blur-sm p-4 shadow">
+            <div className="flex gap-4 items-center justify-center w-auto rounded-full bg-brand-black/60 text-white backdrop-blur-sm p-4 shadow">
               {newMenuItems.map((item, index) => {
-                if (!item._key)
+                if ('isSpacerItem' in item) {
                   return (
-                    <div key={index} className="bg-white h-[1px] w-24"></div>
+                    <div key={`spacer-${index}`} className="bg-white h-[1px] w-24"></div>
                   )
+                }
                 return (
-                  // @ts-ignore
-                  <SanityLink data={item} key={item._key}>
+                  <SanityLink data={item} key={`menu-${index}`}>
                     <span className="uppercase hover:text-brand-orange transition-colors">
                       {item.linkText}
                     </span>
@@ -67,12 +69,16 @@ export default function Navbar(props: NavbarProps) {
             </div>
           </div>
         )}
-        <div className="w-1/6" />
-        <NavMenu
-          navData={data}
-          isMenuOpen={isMenuOpen}
-          setIsMenuOpen={setIsMenuOpen}
-        />
+        
+        <div className="w-1/6 flex justify-end">
+          <div className="block md:hidden">
+            <NavMenu
+              navData={data}
+              isMenuOpen={isMenuOpen}
+              setIsMenuOpen={setIsMenuOpen}
+            />
+          </div>
+        </div>
       </div>
     </nav>
   )
